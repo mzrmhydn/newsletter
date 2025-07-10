2c2732c53122fc517aeeb5547a1ca94709f31e2a
Mazhar Mohyudin <mazharmohyudin27@gmail.com>
2025-07-10 16:14:09 +0500

    changed user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    googleId: {type: String, unique: true},
     email: {type: String, required: true, unique: true},
     verified: {type: Boolean, default: false},
     createdAt: {type: Date, default: Date.now},
