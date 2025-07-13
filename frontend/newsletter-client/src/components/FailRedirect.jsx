fa2870b35ca6f522fda6853793fa373964c6f829
Mazhar Mohyudin <mazharmohyudin27@gmail.com>
2025-07-13 17:00:36 +0500

    styling done

--- frontend/newsletter-client/src/components/FailRedirect.jsx
+++ frontend/newsletter-client/src/components/FailRedirect.jsx
@@ -1,30 +1,19 @@
-import newsletterlogo from "../newsletter logo.jpg"
+import newsletterlogo from "../newsletter logo.png"
 import { useNavigate } from "react-router-dom"
 
 export default function FailRedirect(){
     const navigate = useNavigate()
     return(
         <>
-            <header className="flex justify-center items-center  bg-blue-600 h-32 shadow-md border-b">
-                <img
-                    className="rounded-full w-20 h-20 mr-4"
-                    src={newsletterlogo}
-                    alt="newsletter-logo"
-                />
-                <button
-                    onClick={() => navigate("/")}
-                    className="text-3xl font-bold tracking-tight hover:text-slate-800 transition-colors"
-                >
-                    Newsletter Signup
-                </button>
-            </header>
-            <div className="flex flex-col items-center justify-center min-h-[60vh] text-center px-4">
+        <div className="flex bg-gradient-to-br from-[#2d033b] via-black to-[#1e3a8a] items-center justify-center h-screen px-4" >
+            <div className="flex flex-col items-center justify-center h-screen text-center space-y-4 px-4">
                 <h1 className="text-3xl font-bold text-red-600">❌ Login Failed</h1>
-                <p className="text-gray-700 mt-4">
+                <p className="text-white">
                     Something went wrong with Google Sign-In. Please try again.
                 </p>
                 <a className="text-blue-600 underline"  href="" onClick={() => navigate("/", { replace: true })}>👉🏼 Home Page.</a>
             </div>
+        </div>
         </>
     )
 }
--- frontend/newsletter-client/src/components/SuccessRedirect.jsx
+++ frontend/newsletter-client/src/components/SuccessRedirect.jsx
@@ -1,35 +1,25 @@
 import React from "react";
-import newsletterlogo from "../newsletter logo.jpg"
+import newsletterlogo from "../newsletter logo.png"
 import { useNavigate } from "react-router-dom";
 
 export default function SuccessRedirect(){
     const navigate = useNavigate()
     const baseUrl = process.env.REACT_APP_BASE_URL
     const backendPort = process.env.REACT_APP_BACKEND_PORT
-    //conflicting comment
+    
     return(
         <>
-            <header className="flex justify-center items-center  bg-blue-600 h-32 shadow-md border-b">
-                <img
-                    className="rounded-full w-20 h-20 mr-4"
-                    src={newsletterlogo}
-                    alt="newsletter-logo"
-                />
-                <button
-                    onClick={() => navigate("/")}
-                    className="text-3xl font-bold tracking-tight hover:text-slate-800 transition-colors"
-                >
-                    Newsletter Signup
-                </button>
-            </header>
-            <div className="flex flex-col items-center justify-center min-h-[60vh] px-4 text-center space-y-4">
-                <h1 className="text-3xl font-bold text-green-700">
-                    🥳 Google Login Successful!
-                </h1>
-                <p className="text-lg text-gray-700">
-                    You will now receive our newsletter on your registered Email.
-                </p>
-            </div>
+            <div className="flex bg-gradient-to-br from-[#2d033b] via-black to-[#1e3a8a] items-center justify-center h-screen px-4" >
+                <div className="flex flex-col justify-center items-center space-y-4">
+                    <h1 className="text-3xl font-bold text-green-700">
+                        🥳 Google Login Successful!
+                    </h1>
+                    <p className="text-lg text-white">
+                        You will now receive our newsletter on your registered Email.
+                    </p>
+                    <a className="text-blue-600 underline"  href="" onClick={() => navigate("/", { replace: true })}>👉🏼 Home Page.</a>
+                </div>
+            </div> 
         </>
     )
 }
